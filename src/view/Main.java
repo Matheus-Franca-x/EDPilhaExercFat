5b36eef2847b9faff404e3a8d7abd79f48683732
matheus <matheussss6613@gmail.com>
2023-04-12 23:58:08 -0300

    fim

---
+++ src/view/Main.java
@@ -0,0 +1,20 @@
+package view;
+
+import controlle.FatController;
+
+public class Main 
+{
+	
+	public static void main(String[] args)
+	{
+		FatController pilha = new FatController();
+		
+		pilha.fat(5);
+		
+		
+	}
+	
+	
+	
+
+}
